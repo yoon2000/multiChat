95e2fbe835781711610d716150ace59829f63025
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-05 23:56:56 +0900

    first commit

---
+++ src/main/java/com/ll/multiChat/MultiChatApplication.java
@@ -0,0 +1,13 @@
+package com.ll.multiChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/multiChat/MultiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.multiChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
